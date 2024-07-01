53d75b6cb6b0c3520b563e98835a39e4018c80eb
Lizbeth06 <lizbeth.infante.27@unsch.edu.pe>
2024-07-01 15:48:21 -0500

    Descripción de los cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NosotrosComponent } from './publico/nosotros/nosotros.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NosotrosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/publico/nosotros/nosotros.component.ts
+++ src/app/publico/nosotros/nosotros.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-nosotros',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.css'
 })
 export class NosotrosComponent {
 
+  
 }
+
+  
+
+
