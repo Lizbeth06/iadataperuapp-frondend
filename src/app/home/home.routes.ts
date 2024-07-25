c2ef2ead7188f179455e9a6842f5bbcf6f5df0dc
YONNY <yonny.huarancca.27@unsch.edu.pe>
2024-07-25 16:08:42 -0500

    actualkizar 05-07-2024

--- src/app/home/home.routes.ts
+++ src/app/home/home.routes.ts
@@ -1,15 +1,4 @@
 import { Routes } from '@angular/router';
-import { IndexComponent } from './index/index.component';
-import { NosotrosComponent } from './nosotros/nosotros.component';
-import { CatalogoComponent } from './catalogo/catalogo.component';
-import { ConsultoriaComponent } from './consultoria/consultoria.component';
-import { ServicioTecnicoComponent } from './servicio-tecnico/servicio-tecnico.component';
-import { VentaAccesoriosComponent } from './venta-accesorios/venta-accesorios.component';
-import { VentaSistemasComponent } from './venta-sistemas/venta-sistemas.component';
-import { CursoComponent } from './curso/curso.component';
-import { TallerComponent } from './taller/taller.component';
-import { MicertificadoComponent } from './micertificado/micertificado.component';
-import { DetalleTallerComponent } from './taller/detalle-taller/detalle-taller.component';
 
 export const HOME_ROUTES: Routes = [
     {
